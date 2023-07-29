aff9ebb3b708bd496a74fc0eb64ea6d58b54eb2a
MarichkaTanchyn <mariatanshyn@gmail.com>
2023-07-29 16:09:32 +0200

    .env for db

--- util/database.js
+++ util/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const host = process.env.DB_HOST || 'localhost';
 
-const sequelize = new Sequelize('excelTry', 'maria', 'sa', {
+const sequelize = new Sequelize(
+    process.env.DB_NAME, process.env.DB_LOGIN, process.env.DB_PASSWORD, {
     dialect: 'postgres',
     host: host,
 });
