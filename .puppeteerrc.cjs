6021df9d9af0e585111b2b8f697ccf710cf9423e
MarichkaTanchyn <mariatanshyn@gmail.com>
2023-09-14 22:52:06 +0200

    trying to fix pueppeter problem on host

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,9 +1,7 @@
 const { join } = require('path')
 
 module.exports = {
-    // Changes the cache location for Puppeteer.
     launch: {
-        args: ['--no-sandbox']
-    },
-    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-}
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+};
